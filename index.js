4f5a6814764fa1be5dd24650b66247af22b2bb86
Simen Bekkhus <sbekkhus91@gmail.com>
2019-06-23 22:01:50 +0200

    feat: add a sources option allowing to bypass fs operations (#36)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const V8ToIstanbul = require('./lib/v8-to-istanbul')
 
-module.exports = function (path, wrapperLength) {
-  return new V8ToIstanbul(path, wrapperLength)
+module.exports = function (path, wrapperLength, sources) {
+  return new V8ToIstanbul(path, wrapperLength, sources)
 }
