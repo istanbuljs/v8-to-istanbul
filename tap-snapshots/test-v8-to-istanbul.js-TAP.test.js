ce2ca85a4b7aab34166e03e5a3ee468eb04cf9f0
Moshe Atlow <moshe@testim.io>
2020-07-10 12:43:24 +0300

    add fixure

--- tap-snapshots/test-v8-to-istanbul.js-TAP.test.js
+++ tap-snapshots/test-v8-to-istanbul.js-TAP.test.js
@@ -2569,3 +2569,1081 @@ Object {
   },
 }
 `
+
+exports[`test/v8-to-istanbul.js TAP > must match source-map with multiple source files snapshot 1`] = `
+Object {
+  "b": Object {},
+  "branchMap": Object {},
+  "f": Object {
+    "0": 2,
+    "1": 0,
+    "2": 0,
+    "3": 0,
+    "4": 0,
+    "5": 0,
+  },
+  "fnMap": Object {
+    "0": Object {
+      "decl": Object {
+        "end": Object {
+          "column": 25,
+          "line": 25,
+        },
+        "start": Object {
+          "column": 2,
+          "line": 5,
+        },
+      },
+      "line": 5,
+      "loc": Object {
+        "end": Object {
+          "column": 25,
+          "line": 25,
+        },
+        "start": Object {
+          "column": 2,
+          "line": 5,
+        },
+      },
+      "name": "n",
+    },
+    "1": Object {
+      "decl": Object {
+        "end": Object {
+          "column": 76,
+          "line": 38,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 36,
+        },
+      },
+      "line": 36,
+      "loc": Object {
+        "end": Object {
+          "column": 76,
+          "line": 38,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 36,
+        },
+      },
+      "name": "n.d",
+    },
+    "2": Object {
+      "decl": Object {
+        "end": Object {
+          "column": 65,
+          "line": 47,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 43,
+        },
+      },
+      "line": 43,
+      "loc": Object {
+        "end": Object {
+          "column": 65,
+          "line": 47,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 43,
+        },
+      },
+      "name": "n.r",
+    },
+    "3": Object {
+      "decl": Object {
+        "end": Object {
+          "column": 13,
+          "line": 63,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 55,
+        },
+      },
+      "line": 55,
+      "loc": Object {
+        "end": Object {
+          "column": 13,
+          "line": 63,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 55,
+        },
+      },
+      "name": "n.t",
+    },
+    "4": Object {
+      "decl": Object {
+        "end": Object {
+          "column": 17,
+          "line": 72,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 67,
+        },
+      },
+      "line": 67,
+      "loc": Object {
+        "end": Object {
+          "column": 17,
+          "line": 72,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 67,
+        },
+      },
+      "name": "n.n",
+    },
+    "5": Object {
+      "decl": Object {
+        "end": Object {
+          "column": 120,
+          "line": 76,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 76,
+        },
+      },
+      "line": 76,
+      "loc": Object {
+        "end": Object {
+          "column": 120,
+          "line": 76,
+        },
+        "start": Object {
+          "column": 26,
+          "line": 76,
+        },
+      },
+      "name": "n.o",
+    },
+  },
+  "s": Object {
+    "0": 1,
+    "1": 1,
+    "10": 2,
+    "11": 2,
+    "12": 2,
+    "13": 2,
+    "14": 2,
+    "15": 2,
+    "16": 2,
+    "17": 2,
+    "18": 2,
+    "19": 2,
+    "2": 1,
+    "20": 2,
+    "21": 2,
+    "22": 2,
+    "23": 2,
+    "24": 2,
+    "25": 1,
+    "26": 1,
+    "27": 1,
+    "28": 1,
+    "29": 1,
+    "3": 1,
+    "30": 1,
+    "31": 1,
+    "32": 1,
+    "33": 1,
+    "34": 1,
+    "35": 1,
+    "36": 0,
+    "37": 0,
+    "38": 1,
+    "39": 1,
+    "4": 1,
+    "40": 1,
+    "41": 1,
+    "42": 1,
+    "43": 0,
+    "44": 0,
+    "45": 0,
+    "46": 0,
+    "47": 1,
+    "48": 1,
+    "49": 1,
+    "5": 2,
+    "50": 1,
+    "51": 1,
+    "52": 1,
+    "53": 1,
+    "54": 1,
+    "55": 0,
+    "56": 0,
+    "57": 0,
+    "58": 0,
+    "59": 0,
+    "6": 2,
+    "60": 0,
+    "61": 0,
+    "62": 0,
+    "63": 1,
+    "64": 1,
+    "65": 1,
+    "66": 1,
+    "67": 0,
+    "68": 0,
+    "69": 0,
+    "7": 2,
+    "70": 0,
+    "71": 0,
+    "72": 1,
+    "73": 1,
+    "74": 1,
+    "75": 1,
+    "76": 1,
+    "77": 1,
+    "78": 1,
+    "79": 1,
+    "8": 2,
+    "80": 1,
+    "81": 1,
+    "82": 1,
+    "9": 2,
+  },
+  "statementMap": Object {
+    "0": Object {
+      "end": Object {
+        "column": 21,
+        "line": 1,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 1,
+      },
+    },
+    "1": Object {
+      "end": Object {
+        "column": 28,
+        "line": 2,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 2,
+      },
+    },
+    "10": Object {
+      "end": Object {
+        "column": 53,
+        "line": 11,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 11,
+      },
+    },
+    "11": Object {
+      "end": Object {
+        "column": 46,
+        "line": 12,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 12,
+      },
+    },
+    "12": Object {
+      "end": Object {
+        "column": 16,
+        "line": 13,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 13,
+      },
+    },
+    "13": Object {
+      "end": Object {
+        "column": 13,
+        "line": 14,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 14,
+      },
+    },
+    "14": Object {
+      "end": Object {
+        "column": 15,
+        "line": 15,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 15,
+      },
+    },
+    "15": Object {
+      "end": Object {
+        "column": 5,
+        "line": 16,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 16,
+      },
+    },
+    "16": Object {
+      "end": Object {
+        "column": 0,
+        "line": 17,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 17,
+      },
+    },
+    "17": Object {
+      "end": Object {
+        "column": 33,
+        "line": 18,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 18,
+      },
+    },
+    "18": Object {
+      "end": Object {
+        "column": 87,
+        "line": 19,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 19,
+      },
+    },
+    "19": Object {
+      "end": Object {
+        "column": 0,
+        "line": 20,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 20,
+      },
+    },
+    "2": Object {
+      "end": Object {
+        "column": 0,
+        "line": 3,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 3,
+      },
+    },
+    "20": Object {
+      "end": Object {
+        "column": 31,
+        "line": 21,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 21,
+      },
+    },
+    "21": Object {
+      "end": Object {
+        "column": 19,
+        "line": 22,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 22,
+      },
+    },
+    "22": Object {
+      "end": Object {
+        "column": 0,
+        "line": 23,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 23,
+      },
+    },
+    "23": Object {
+      "end": Object {
+        "column": 38,
+        "line": 24,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 24,
+      },
+    },
+    "24": Object {
+      "end": Object {
+        "column": 25,
+        "line": 25,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 25,
+      },
+    },
+    "25": Object {
+      "end": Object {
+        "column": 3,
+        "line": 26,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 26,
+      },
+    },
+    "26": Object {
+      "end": Object {
+        "column": 0,
+        "line": 27,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 27,
+      },
+    },
+    "27": Object {
+      "end": Object {
+        "column": 0,
+        "line": 28,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 28,
+      },
+    },
+    "28": Object {
+      "end": Object {
+        "column": 52,
+        "line": 29,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 29,
+      },
+    },
+    "29": Object {
+      "end": Object {
+        "column": 34,
+        "line": 30,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 30,
+      },
+    },
+    "3": Object {
+      "end": Object {
+        "column": 25,
+        "line": 4,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 4,
+      },
+    },
+    "30": Object {
+      "end": Object {
+        "column": 0,
+        "line": 31,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 31,
+      },
+    },
+    "31": Object {
+      "end": Object {
+        "column": 28,
+        "line": 32,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 32,
+      },
+    },
+    "32": Object {
+      "end": Object {
+        "column": 43,
+        "line": 33,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 33,
+      },
+    },
+    "33": Object {
+      "end": Object {
+        "column": 0,
+        "line": 34,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 34,
+      },
+    },
+    "34": Object {
+      "end": Object {
+        "column": 47,
+        "line": 35,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 35,
+      },
+    },
+    "35": Object {
+      "end": Object {
+        "column": 59,
+        "line": 36,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 36,
+      },
+    },
+    "36": Object {
+      "end": Object {
+        "column": 46,
+        "line": 37,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 37,
+      },
+    },
+    "37": Object {
+      "end": Object {
+        "column": 76,
+        "line": 38,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 38,
+      },
+    },
+    "38": Object {
+      "end": Object {
+        "column": 4,
+        "line": 39,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 39,
+      },
+    },
+    "39": Object {
+      "end": Object {
+        "column": 4,
+        "line": 40,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 40,
+      },
+    },
+    "4": Object {
+      "end": Object {
+        "column": 42,
+        "line": 5,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 5,
+      },
+    },
+    "40": Object {
+      "end": Object {
+        "column": 0,
+        "line": 41,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 41,
+      },
+    },
+    "41": Object {
+      "end": Object {
+        "column": 33,
+        "line": 42,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 42,
+      },
+    },
+    "42": Object {
+      "end": Object {
+        "column": 45,
+        "line": 43,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 43,
+      },
+    },
+    "43": Object {
+      "end": Object {
+        "column": 60,
+        "line": 44,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 44,
+      },
+    },
+    "44": Object {
+      "end": Object {
+        "column": 76,
+        "line": 45,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 45,
+      },
+    },
+    "45": Object {
+      "end": Object {
+        "column": 4,
+        "line": 46,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 46,
+      },
+    },
+    "46": Object {
+      "end": Object {
+        "column": 65,
+        "line": 47,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 47,
+      },
+    },
+    "47": Object {
+      "end": Object {
+        "column": 4,
+        "line": 48,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 48,
+      },
+    },
+    "48": Object {
+      "end": Object {
+        "column": 0,
+        "line": 49,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 49,
+      },
+    },
+    "49": Object {
+      "end": Object {
+        "column": 35,
+        "line": 50,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 50,
+      },
+    },
+    "5": Object {
+      "end": Object {
+        "column": 0,
+        "line": 6,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 6,
+      },
+    },
+    "50": Object {
+      "end": Object {
+        "column": 47,
+        "line": 51,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 51,
+      },
+    },
+    "51": Object {
+      "end": Object {
+        "column": 56,
+        "line": 52,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 52,
+      },
+    },
+    "52": Object {
+      "end": Object {
+        "column": 50,
+        "line": 53,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 53,
+      },
+    },
+    "53": Object {
+      "end": Object {
+        "column": 36,
+        "line": 54,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 54,
+      },
+    },
+    "54": Object {
+      "end": Object {
+        "column": 49,
+        "line": 55,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 55,
+      },
+    },
+    "55": Object {
+      "end": Object {
+        "column": 51,
+        "line": 56,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 56,
+      },
+    },
+    "56": Object {
+      "end": Object {
+        "column": 29,
+        "line": 57,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 57,
+      },
+    },
+    "57": Object {
+      "end": Object {
+        "column": 89,
+        "line": 58,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 58,
+      },
+    },
+    "58": Object {
+      "end": Object {
+        "column": 32,
+        "line": 59,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 59,
+      },
+    },
+    "59": Object {
+      "end": Object {
+        "column": 29,
+        "line": 60,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 60,
+      },
+    },
+    "6": Object {
+      "end": Object {
+        "column": 33,
+        "line": 7,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 7,
+      },
+    },
+    "60": Object {
+      "end": Object {
+        "column": 76,
+        "line": 61,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 61,
+      },
+    },
+    "61": Object {
+      "end": Object {
+        "column": 151,
+        "line": 62,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 62,
+      },
+    },
+    "62": Object {
+      "end": Object {
+        "column": 13,
+        "line": 63,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 63,
+      },
+    },
+    "63": Object {
+      "end": Object {
+        "column": 4,
+        "line": 64,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 64,
+      },
+    },
+    "64": Object {
+      "end": Object {
+        "column": 0,
+        "line": 65,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 65,
+      },
+    },
+    "65": Object {
+      "end": Object {
+        "column": 73,
+        "line": 66,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 66,
+      },
+    },
+    "66": Object {
+      "end": Object {
+        "column": 44,
+        "line": 67,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 67,
+      },
+    },
+    "67": Object {
+      "end": Object {
+        "column": 45,
+        "line": 68,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 68,
+      },
+    },
+    "68": Object {
+      "end": Object {
+        "column": 57,
+        "line": 69,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 69,
+      },
+    },
+    "69": Object {
+      "end": Object {
+        "column": 51,
+        "line": 70,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 70,
+      },
+    },
+    "7": Object {
+      "end": Object {
+        "column": 35,
+        "line": 8,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 8,
+      },
+    },
+    "70": Object {
+      "end": Object {
+        "column": 46,
+        "line": 71,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 71,
+      },
+    },
+    "71": Object {
+      "end": Object {
+        "column": 17,
+        "line": 72,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 72,
+      },
+    },
+    "72": Object {
+      "end": Object {
+        "column": 4,
+        "line": 73,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 73,
+      },
+    },
+    "73": Object {
+      "end": Object {
+        "column": 0,
+        "line": 74,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 74,
+      },
+    },
+    "74": Object {
+      "end": Object {
+        "column": 41,
+        "line": 75,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 75,
+      },
+    },
+    "75": Object {
+      "end": Object {
+        "column": 120,
+        "line": 76,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 76,
+      },
+    },
+    "76": Object {
+      "end": Object {
+        "column": 0,
+        "line": 77,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 77,
+      },
+    },
+    "77": Object {
+      "end": Object {
+        "column": 28,
+        "line": 78,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 78,
+      },
+    },
+    "78": Object {
+      "end": Object {
+        "column": 29,
+        "line": 79,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 79,
+      },
+    },
+    "79": Object {
+      "end": Object {
+        "column": 0,
+        "line": 80,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 80,
+      },
+    },
+    "8": Object {
+      "end": Object {
+        "column": 46,
+        "line": 9,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 9,
+      },
+    },
+    "80": Object {
+      "end": Object {
+        "column": 0,
+        "line": 81,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 81,
+      },
+    },
+    "81": Object {
+      "end": Object {
+        "column": 41,
+        "line": 82,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 82,
+      },
+    },
+    "82": Object {
+      "end": Object {
+        "column": 56,
+        "line": 83,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 83,
+      },
+    },
+    "9": Object {
+      "end": Object {
+        "column": 4,
+        "line": 10,
+      },
+      "start": Object {
+        "column": 0,
+        "line": 10,
+      },
+    },
+  },
+}
+`
---
+++ test/fixtures/multi-source-map.js
@@ -0,0 +1,163 @@
+module.exports = {
+  describe: 'source-map with multiple source files',
+  coverageV8: {
+    "scriptId": "57",
+    "url": "./test/fixtures/scripts/sourcemap-multisource.js",
+    "functions": [
+      {
+        "functionName": "",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 1,
+            "endOffset": 1658,
+            "startOffset": 0
+          }
+        ]
+      },
+      {
+        "functionName": "",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 1,
+            "endOffset": 910,
+            "startOffset": 1
+          }
+        ]
+      },
+      {
+        "functionName": "n",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 2,
+            "endOffset": 156,
+            "startOffset": 22
+          }
+        ]
+      },
+      {
+        "functionName": "n.d",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 0,
+            "endOffset": 247,
+            "startOffset": 172
+          }
+        ]
+      },
+      {
+        "functionName": "n.r",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 0,
+            "endOffset": 422,
+            "startOffset": 252
+          }
+        ]
+      },
+      {
+        "functionName": "n.t",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 0,
+            "endOffset": 714,
+            "startOffset": 427
+          }
+        ]
+      },
+      {
+        "functionName": "n.n",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 0,
+            "endOffset": 825,
+            "startOffset": 719
+          }
+        ]
+      },
+      {
+        "functionName": "n.o",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 0,
+            "endOffset": 893,
+            "startOffset": 830
+          }
+        ]
+      },
+      {
+        "functionName": "",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 1,
+            "endOffset": 1485,
+            "startOffset": 912
+          }
+        ]
+      },
+      {
+        "functionName": "",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 1,
+            "endOffset": 1182,
+            "startOffset": 1152
+          }
+        ]
+      },
+      {
+        "functionName": "",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 0,
+            "endOffset": 1353,
+            "startOffset": 1297
+          }
+        ]
+      },
+      {
+        "functionName": "t.counter",
+        "isBlockCoverage": true,
+        "ranges": [
+          {
+            "count": 1,
+            "endOffset": 1454,
+            "startOffset": 1395
+          }
+        ]
+      },
+      {
+        "functionName": "t.setCounter",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 0,
+            "endOffset": 1484,
+            "startOffset": 1468
+          }
+        ]
+      },
+      {
+        "functionName": "",
+        "isBlockCoverage": false,
+        "ranges": [
+          {
+            "count": 1,
+            "endOffset": 1605,
+            "startOffset": 1486
+          }
+        ]
+      }
+    ]
+  }
+}
