43c85d1e75bb52f908bdfcd7b8f1ad30c946a7d2
AriPerkkio <ari.perkkio@gmail.com>
2024-03-23 10:08:52 +0200

    fix: ignore hint to exclude lines

--- test/fixtures/scripts/ignored.lines.js
+++ test/fixtures/scripts/ignored.lines.js
@@ -1,4 +1,13 @@
-/* c8 ignore next 3 */
 function sum(a, b) {
+  if(a === 2 && b === 2) {
+    return 4;
+  }
+
+  /* v8 ignore start */ // Line 6
+  if (a === '10') {
+    return add(a, b)
+  }
+  /* v8 ignore stop */ // Line 10
+
   return a + b;
 };
